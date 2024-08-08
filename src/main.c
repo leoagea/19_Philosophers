c39d1422d5ad1a9ae6d6962b4eda984e1bbf29db
leoagea <leoagea@gmail.com>
2024-08-08 17:56:19 +0200

    Update : add thread_create and join_thread

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
+/*   By: lagea <lagea@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 13:25:24 by lagea             #+#    #+#             */
-/*   Updated: 2024/08/08 00:34:48 by lagea            ###   ########.fr       */
+/*   Updated: 2024/08/08 16:46:32 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,18 @@ size_t	get_current_time(void)
 	return (time.tv_sec * 1000 + time.tv_usec / 1000);
 }
 
+void ft_join_thread(t_data *data)
+{
+    int i;
+
+    i = -1;
+    while (++i < data->nb_philo)
+        pthread_join(data->philos[i].thread, NULL);    
+}
+
 int main(int ac, char **av)
 {
+    int i;
     (void) av;
     t_data data;
     
@@ -33,6 +43,14 @@ int main(int ac, char **av)
     printf("nb philo : %d\ntime to die : %d\ntime to eat : %d\ntime to sleep : %d\nnb of times each philo must eat : %d\n", data.nb_philo, data.t_to_die, data.t_to_eat, data.t_to_sleep, data.nb_must_eat);
     if (init_philo(&data)  || init_fork(&data))
         printf("Error Allocation array philos or fork\n");
+    i = -1;
+    while (++i < data.nb_philo)
+    {
+        if (pthread_create(&data.philos[i].thread, NULL, routine, (void *)&data.philos[i]))
+            return (ft_exit(&data), 1);
+    }
+    
+    ft_join_thread(&data);
     ft_exit(&data);
     return 0;   
 }
