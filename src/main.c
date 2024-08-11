0e422d0daceda3e26d70dc79c7cfe7b2db981f53
Leo Agea <leoagea@gmail.com>
2024-08-12 01:53:11 +0200

    Update : case one philo

--- src/main.c
+++ src/main.c
@@ -6,11 +6,23 @@
 /*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 13:25:24 by lagea             #+#    #+#             */
-/*   Updated: 2024/08/12 01:17:22 by lagea            ###   ########.fr       */
+/*   Updated: 2024/08/12 01:48:16 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
+
+static int	case_one(t_data *data)
+{
+	data->start_time = get_time();
+	if (pthread_create(&data->thread[0], NULL, &routine, &data->philos[0]))
+		return (ft_error(ERR_THREAD, data));
+	pthread_detach(data->thread[0]);
+	while (data->dead == 0)
+		ft_usleep(0);
+	ft_exit(data);
+	return (0);
+}
 
 int main(int ac, char **av)
 {
@@ -23,4 +35,10 @@ int main(int ac, char **av)
 		return (1);
     if (init(&data, av, ac))
 		return (1);
+    if (data.philo_num == 1)
+		return (case_one(&data));
+    if (loop_thread(&data))
+		return (1);
+    ft_exit(&data);
+	return (0);
 }
