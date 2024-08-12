060b51633255782b7bf426f198816493fdc6ee2c
leoagea <leoagea@gmail.com>
2024-08-12 17:40:03 +0200

    Fix : data race case : ./philo 1 800 200 200

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
+/*   By: lagea <lagea@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 13:25:24 by lagea             #+#    #+#             */
-/*   Updated: 2024/08/12 01:57:16 by lagea            ###   ########.fr       */
+/*   Updated: 2024/08/12 17:37:10 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,17 @@ static int	case_one(t_data *data)
 	if (pthread_create(&data->thread[0], NULL, &routine, &data->philos[0]))
 		return (ft_error(ERR_THREAD, data));
 	pthread_detach(data->thread[0]);
-	while (data->dead == 0)
+	while (1)
+	{
+		pthread_mutex_lock(&data->death_lock);
+    	if (data->dead)
+    	{
+    	    pthread_mutex_unlock(&data->death_lock);
+    	    break;
+    	}
+    	pthread_mutex_unlock(&data->death_lock);
 		ft_usleep(0);
+	}
 	ft_exit(data);
 	return (0);
 }
