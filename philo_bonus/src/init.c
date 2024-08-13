4dab293817911fb300193cf9880a2beefca3e5c9
leoagea <leoagea@gmail.com>
2024-08-13 16:09:23 +0200

    Update : parsing and init

--- philo_bonus/src/init.c
+++ philo_bonus/src/init.c
@@ -6,61 +6,17 @@
 /*   By: lagea <lagea@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 13:24:51 by lagea             #+#    #+#             */
-/*   Updated: 2024/08/12 16:12:31 by lagea            ###   ########.fr       */
+/*   Updated: 2024/08/13 13:46:58 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
 
-static void	init_philos(t_data *data)
+static int init_philo(t_data *data)
 {
-	int	i;
-
-	i = 0;
-	while (i < data->philo_num)
-	{
-		data->philos[i].data = data;
-		data->philos[i].id = i + 1;
-		data->philos[i].time_to_die = data->death_time;
-		data->philos[i].eat_cont = 0;
-		data->philos[i].eating = 0;
-		data->philos[i].status = 0;
-		pthread_mutex_init(&data->philos[i].lock, NULL);
-		i++;
-	}
-}
-
-int	init_forks(t_data *data)
-{
-	int	i;
-
-	i = -1;
-	while (++i < data->philo_num)
-		pthread_mutex_init(&data->forks[i], NULL);
-	i = 0;
-	data->philos[0].l_fork = &data->forks[0];
-	data->philos[0].r_fork = &data->forks[data->philo_num - 1];
-	i = 1;
-	while (i < data->philo_num)
-	{
-		data->philos[i].l_fork = &data->forks[i];
-		data->philos[i].r_fork = &data->forks[i - 1];
-		i++;
-	}
-	return (0);
-}
-
-static int	init_malloc(t_data *data)
-{
-	data->thread = malloc(sizeof(pthread_t) * data->philo_num);
-	if (!data->thread)
-		return (ft_error(ERR_ALLOC_1, data));
-	data->forks = malloc(sizeof(pthread_mutex_t) * data->philo_num);
-	if (!data->forks)
-		return (ft_error(ERR_ALLOC_2, data));
 	data->philos = malloc(sizeof(t_philo) * data->philo_num);
 	if (!data->philos)
-		return (ft_error(ERR_ALLOC_3, data));
+		return (ft_error(ERR_ALLOC_3, data), 1);
 	return (0);
 }
 
@@ -76,12 +32,15 @@ static int	init_data(t_data *data, char **av, int ac)
 		data->meals_nb = -1;
 	if (data->philo_num <= 0 || data->philo_num > 200 || data->death_time < 0
 		|| data->eat_time < 0 || data->sleep_time < 0)
-		return (ft_error(ERR_2, NULL));
+		return (ft_error(ERR_2, NULL), 1);
 	data->dead = 0;
 	data->finished = 0;
-	pthread_mutex_init(&data->write, NULL);
-	pthread_mutex_init(&data->lock, NULL);
-	pthread_mutex_init(&data->death_lock, NULL);
+	data->fork = sem_open("fork", O_CREAT, 0700, data->philo_num);
+	if (data->fork == SEM_FAILED);
+		return (ft_error(ERR_SEMA_1, data), 1);
+	data->write = sem_open("write", O_CREAT, 0700, 1);
+	if (data->write == SEM_FAILED);
+		return (ft_error(ERR_SEMA_1, data), 1);
 	return (0);
 }
 
@@ -89,10 +48,7 @@ int	init(t_data *data, char **av, int ac)
 {
 	if (init_data(data, av, ac))
 		return (1);
-	if (init_malloc(data))
+	if (init_philo(data))
 		return (1);
-	if (init_forks(data))
-		return (1);
-	init_philos(data);
 	return (0);
 }
