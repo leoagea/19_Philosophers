3e9ac29beb442595bda66b65ae2b476d4f1b2338
leoagea <leoagea@gmail.com>
2024-08-13 17:39:19 +0200

    Done on 13/08 5:40 pm

--- philo_bonus/inc/philo.h
+++ philo_bonus/inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: lagea <lagea@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 13:25:35 by lagea             #+#    #+#             */
-/*   Updated: 2024/08/12 17:00:51 by lagea            ###   ########.fr       */
+/*   Updated: 2024/08/13 13:53:30 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,14 @@
 
 # include <limits.h>
 # include <pthread.h>
+#include <semaphore.h>
 # include <stdatomic.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <sys/time.h> //struct timeval
 # include <unistd.h>   //sleep
+#include <fcntl.h>
+#include <signal.h>
 
 # define RED "\033[1;31m"   // DIED
 # define BLUE "\033[34;01m" // FORK
@@ -32,6 +35,7 @@
 # define ERR_NB_ARG "Error: Wrong number of arguments"
 # define ERR_1 "Error: Input invalid character"
 # define ERR_2 "Error: Input invalid values"
+# define ERR_SEMA_1 "Error: Open semaphore"
 # define ERR_ALLOC_1 "Error: Allocation pthread"
 # define ERR_ALLOC_2 "Error: Allocation mutex"
 # define ERR_ALLOC_3 "Error: Allocation struct philo"
@@ -51,27 +55,23 @@ typedef struct s_philo
 	int				status;
 	int				eating;
 	uint64_t		time_to_die;
-	pthread_mutex_t	lock;
-	pthread_mutex_t	*r_fork;
-	pthread_mutex_t	*l_fork;
+	
 }					t_philo;
 
 typedef struct s_data
 {
-	pthread_t		*thread;
 	int				philo_num;
 	int				meals_nb;
 	int				dead;
 	int				finished;
+	int				*pid;
 	t_philo			*philos;
 	u_int64_t		death_time;
 	u_int64_t		eat_time;
 	u_int64_t		sleep_time;
 	u_int64_t		start_time;
-	pthread_mutex_t	*forks;
-	pthread_mutex_t	lock;
-	pthread_mutex_t	write;
-	pthread_mutex_t	death_lock;
+	sem_t			fork;
+	sem_t			write;
 }					t_data;
 
 /*---------------------------Action--------------------------*/
--- philo_bonus/src/utils_2.c
+++ philo_bonus/src/utils_2.c
@@ -6,7 +6,7 @@
 /*   By: lagea <lagea@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 17:59:51 by lagea             #+#    #+#             */
-/*   Updated: 2024/08/12 16:32:22 by lagea            ###   ########.fr       */
+/*   Updated: 2024/08/13 14:38:43 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,9 +35,9 @@ void	print(char *str, t_philo *philo)
 {
 	u_int64_t	time;
 
-	pthread_mutex_lock(&philo->data->write);
+	sem_wait(&philo->data->write);
 	time = get_time() - philo->data->start_time;
-	pthread_mutex_lock(&philo->data->death_lock);
+	// pthread_mutex_lock(&philo->data->death_lock);
 	if (ft_strcmp(DEAD, str) == 0 && philo->data->dead == 0)
 	{
 		printf("[%llu] [%d] " RED "%s" RESET "\n", time, philo->id, str);
@@ -45,6 +45,6 @@ void	print(char *str, t_philo *philo)
 	}
 	if (!philo->data->dead)
 		printf("[%llu] [%d] " BLUE "%s" RESET "\n", time, philo->id, str);
-	pthread_mutex_unlock(&philo->data->death_lock);
-	pthread_mutex_unlock(&philo->data->write);
+	// pthread_mutex_unlock(&philo->data->death_lock);
+	sem_post(&philo->data->write);
 }
