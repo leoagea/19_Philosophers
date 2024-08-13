22c9167ae9d0779660a2828446d7951165323c19
Leo Agea <leoagea@gmail.com>
2024-08-14 00:39:38 +0200

    Update : init, init sema

--- philo_bonus/src/init.c
+++ philo_bonus/src/init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lagea <lagea@student.42.fr>                +#+  +:+       +#+        */
+/*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 13:24:51 by lagea             #+#    #+#             */
-/*   Updated: 2024/08/13 13:46:58 by lagea            ###   ########.fr       */
+/*   Updated: 2024/08/14 00:37:41 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,25 @@ static int init_philo(t_data *data)
 	return (0);
 }
 
+static int init_sema(t_data *data)
+{
+	sem_unlink("fork");
+	sem_unlink("write");
+	data->fork = sem_open("fork", O_CREAT, 0700, data->philo_num);
+	data->write = sem_open("write", O_CREAT, 0700, 1);
+	if (data->fork == SEM_FAILED || data->write == SEM_FAILED)
+		return (ft_error(ERR_SEMA_1, data), 1);
+	return (0);
+}
+
 static int	init_data(t_data *data, char **av, int ac)
 {
 	data->philo_num = ft_atoi(av[1]);
 	data->death_time = (u_int64_t)ft_atoi(av[2]);
 	data->eat_time = (u_int64_t)ft_atoi(av[3]);
 	data->sleep_time = (u_int64_t)ft_atoi(av[4]);
+	data->start_time = get_time();
+	data->dead = 0;
 	if (ac == 6)
 		data->meals_nb = ft_atoi(av[5]);
 	else
@@ -33,14 +46,9 @@ static int	init_data(t_data *data, char **av, int ac)
 	if (data->philo_num <= 0 || data->philo_num > 200 || data->death_time < 0
 		|| data->eat_time < 0 || data->sleep_time < 0)
 		return (ft_error(ERR_2, NULL), 1);
-	data->dead = 0;
 	data->finished = 0;
-	data->fork = sem_open("fork", O_CREAT, 0700, data->philo_num);
-	if (data->fork == SEM_FAILED);
-		return (ft_error(ERR_SEMA_1, data), 1);
-	data->write = sem_open("write", O_CREAT, 0700, 1);
-	if (data->write == SEM_FAILED);
-		return (ft_error(ERR_SEMA_1, data), 1);
+	if (init_sema(data))
+		return (delete_sema(data), 1);
 	return (0);
 }
 
